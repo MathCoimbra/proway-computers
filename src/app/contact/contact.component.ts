645fa8a58c1744ef7f0b668c4fbf1809db8e39eb
MathCoimbra <matheuscoimbra35@gmail.com>
2023-01-09 12:53:40 -0300

    feat: add form with validations
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+
+  /* validações para o preenchimento do formulário de contato */
+  formContact = this.fb.group({
+    nome: ["", [
+      Validators.minLength(4),
+      Validators.required
+    ]],
+    assunto: ["", [
+      Validators.minLength(10),
+      Validators.required
+    ]],
+    telefone: ["", [
+      Validators.minLength(11),
+      Validators.required
+    ]],
+    email: ["", [
+      Validators.email,
+      Validators.required
+    ]],
+    mensagem: ["", [
+      Validators.minLength(20),
+      Validators.required
+    ]],
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  sendForm() {
+    alert('A mensagem foi enviada!');
+    this.formContact.reset();
   }
 
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,9 @@ import { ContactComponent } from './contact.component';
   ],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ]
 })
 export class ContactModule { }
